7bc60acc9490ac03205cb68313cd6f4fdcd5edb2
Polina Lazareva <p.dubrovina@mail.ru>
2023-04-04 15:50:07 +0300

    Добавили рандомные числа

--- C#_GB/Lecture1_Sum/Program.cs
+++ C#_GB/Lecture1_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); //то есть рандомное число от 1 до 9
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB);
  
