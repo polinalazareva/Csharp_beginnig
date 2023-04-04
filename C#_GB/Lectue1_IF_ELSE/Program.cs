a484dc9d7d9d0ba54e61718bfe03b8640666d26a
Polina Lazareva <p.dubrovina@mail.ru>
2023-04-04 16:00:53 +0300

    Особое приветствие

---
+++ C#_GB/Lectue1_IF_ELSE/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "полина")
+{
+Console.Write("О Боже как я тебя ждал ПОЛИНА!");
+}
+else
+{
+Console.Write("Привет, ");
+Console.Write(username);
+}
